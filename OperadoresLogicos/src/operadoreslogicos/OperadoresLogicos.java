bcddf22b6be3868d50adaeec83bb43ceabdf61c6
LitmanEnriquePC <1504792@senati.pe>
2024-04-23 13:46:24 -0500

    Operadores logicos

---
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,21 @@
+
+package operadoreslogicos;
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean esVerdadero = true;
+        boolean esFalso = false;
+        
+        //Operador AND (&&)
+        boolean resultadoAnd = esVerdadero && esFalso;    
+        System.out.println("AND: "+ resultadoAnd);
+        
+        //Operador OR (||)
+        boolean resultadoOR = esVerdadero || esFalso;
+        System.out.println("OR: "+ resultadoOR);
+        
+        //Operador NOT (!)
+        boolean resultadoNOT = !esVerdadero;
+        System.out.println("NOT "+ resultadoNOT);     
+    }
+    
+}
