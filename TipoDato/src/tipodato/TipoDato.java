ab1e11668fa4e6ff968b3c7208c338f87328ef5d
LitmanEnriquePC <1504792@senati.pe>
2024-04-23 13:20:15 -0500

    first commit

---
+++ TipoDato/src/tipodato/TipoDato.java
@@ -0,0 +1,37 @@
+package tipodato;
+public class TipoDato {
+    public static void main(String[] args) {
+        // Cadenas O String
+        // Declarando
+        String Nombre;
+        int Edad;
+        boolean Estado;
+        double Saldo;
+        Object TodoTipoDato;
+        // Asignando
+        Nombre = "Litman Enrique Portillo Colque";
+        Edad = 18;
+        Saldo = 1001.34;
+        TodoTipoDato = "Cadena";    
+        TodoTipoDato = 2;
+        TodoTipoDato = true;
+        TodoTipoDato = 23.33;
+        // Asignando
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("Edad: " + Edad);
+        System.out.println("Saldo: " + Saldo);
+        System.out.println("Dinamico: " + TodoTipoDato);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
