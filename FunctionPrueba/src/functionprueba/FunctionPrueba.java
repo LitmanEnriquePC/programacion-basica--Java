b5d9c4b169228820d41cf5e75f1a53363b919ffc
LitmanEnriquePC <1504792@senati.pe>
2024-04-24 13:29:11 -0500

    OperadoresConCadenas 24_04_2024

---
+++ FunctionPrueba/src/functionprueba/FunctionPrueba.java
@@ -0,0 +1,28 @@
+package functionprueba;
+import java.util.Scanner;
+public class FunctionPrueba {
+    public static void main(String[] args) {
+        // Defino un ususario y una contraseña 
+        String Usuario = "System";
+        String Contraseña = "admin123";
+        
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Ingrese su usuario:");
+            Usuario = scanner.nextLine();
+        System.out.println("Ingrese su usuario: ");
+            Contraseña = scanner.nextLine();
+            
+        if (Usuario.equals("System") && (Contraseña.equals("admin123"))) {
+            System.out.print("Inicio de sesion Exitoso");
+        } else {
+            System.out.print("Incorrect Password");
+            
+        
+        
+        }
+
+     
+    }
+    
+}
---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,48 @@
+
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int variableUno = 50;
+        int variableDos = 20;
+        
+        
+        // Incremento
+        //variableUno = variableUno + 1;
+         //variableUno++;
+         //System.out.println("Increment: "+ variableUno);
+        
+        //Decremento
+        //variableDos = variableDos - 1;
+         //variableDos-- ;
+         //System.out.println("Decremento: "+variableDos);
+        
+         
+        //SUMA
+        int ResultadoSuma = variableUno + variableDos;
+        System.out.println("Suma: " + ResultadoSuma);
+        
+        //RESTA
+        int ResultadoResta = variableUno - variableDos;
+        System.out.println("Resta: " + ResultadoResta);
+        
+        //MULTIPLICACION
+        int ResultadoMultiplicacion = variableUno * variableDos;
+        System.out.println("Multiplicacion: " + ResultadoMultiplicacion);
+        
+        //MULTIPLICACION
+        int ResultadoDivision = variableUno / variableDos;
+        System.out.println("Division: " + ResultadoDivision);
+        
+        //MODULO
+        int ResultadoModulo = variableUno % variableDos;
+        System.out.println("Modulo: " + ResultadoModulo);
+         
+        
+        
+        
+
+    }
+    
+}
